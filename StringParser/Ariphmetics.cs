630ff56ca7585018b73bb22eeb3d14e343670c96
ghimik <bolit006.br0m@gmail.com>
2023-11-15 23:44:53 +0300

    недопулл реквест

---
+++ StringParser/Ariphmetics.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParser
+{
+    /// <summary>
+    /// Модуль, обеспечивающий некоторые особые математические
+    /// возможности по работе с функциями
+    /// </summary>
+    public static class Ariphmetics
+    {
+        /// <summary>
+        /// Возвращает значение интеграла даной функции на данном отрезке
+        /// полученный методом Симпсона
+        /// </summary>
+        /// <param name="f">Интегрируемая функция</param>
+        /// <param name="start">Начало интегрируемого отрезка</param>
+        /// <param name="end">Конец интегрируемого отрезка</param>
+        /// <param name="numSteps">Количество шагов при интеграции</param>
+        /// <returns>Значение интеграла даной функции на данном отрезке</returns>
+        public static double Integrate(Func<double, double> f, double start, double end,
+            int numSteps)
+        {
+            double stepSize = (end - start) / numSteps;
+            double result = 0.0;
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                double x0 = start + i * stepSize;
+                double x1 = start + (i + 1) * stepSize;
+                double xMid = (x0 + x1) / 2.0;
+
+                result += stepSize / 6 * (f(x0) + 4 * f(xMid) + f(x1));
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// Вычисляет факториал от данного числа
+        /// </summary>
+        /// <param name="a">Данное числа</param>
+        /// <returns>Факториал данного числа</returns>
+        public static double Factorial(double a)
+        {
+            double res = 1;
+            for (double i = 2; i <= a; i++)
+                res *= i;
+            return res;
+        }
+
+        /// <summary>
+        /// Вычисляет значение дзета-функции от данного числа
+        /// </summary>
+        /// <param name="a">Данное числа</param>
+        /// <returns>Значение дзета-функции данного числа</returns>
+        public static double Zeta(double a)
+        {
+            return a;
+        }
+
+        /// Метод, находящий количество простых чисел, меньших либо равных
+        /// действительному числу
+        /// </summary>
+        /// <param name="x">действительное число</param>
+        /// <returns>возвращает кол-во простых чисел</returns>
+        public static double Pi(double x)
+        {
+            return GetPrimeNumbersCount((int)x);
+        }
+        public static int GetPrimeNumbersCount(int n)
+        {
+            int count = 0;
+            bool[] isNotPrime = new bool[n + 1];
+            for (int j = 2; j * j <= n; j++)
+                if (!isNotPrime[j])
+                    for (int i = j * j; i <= n; i += j)
+                        isNotPrime[i] = true;
+            int start = 2;
+            for (int i = start; i <= n; i++)
+                if (!isNotPrime[i])
+                    count++;
+            return count;
+        }
+        /// <summary>
+        /// Вычисляет значение гамма-функции от данного числа, с заданной
+        /// точностью(метод Аппроксимация Ланцоша)
+        /// </summary>
+        /// <param name="a">Данное число в границах [1:171]</param>
+        /// <param name="r">кол-во знаков после запятой</param>
+        /// <returns>Значение гамма-функции данного числа</returns>
+        public static double Gamma(double a, int r)
+        {
+            double[] coeff = {
+            76.18009172947146, -86.50532032941677, 24.01409824083091,
+            -1.231739572450155, 0.001208650973866179, -0.000005395239384953}; // массив коэффициентов при g = 5
+            double y = a;
+            double temp = a + 5.5;
+            temp -= (a + 0.5) * Math.Log(temp);
+            double ser = 1.000000000190015;// первый коэффициент
+            for (int i = 0; i <= coeff.Length - 1; ++i)
+                ser += coeff[i] / ++y;
+            return Math.Round(Math.Exp(-temp + Math.Log(2.5066282746310005) + Math.Log(ser / a)),r);
+        }
+    }
+}
---
+++ StringParser/ExtentedToken.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParser
+{
+    /// <summary>
+    /// Класс, представляющий собой расширенный токен - 
+    /// произвольное выражение, значение которого можно вычислить,
+    /// а также либо скобки либо бинарный оператор
+    /// </summary>
+    internal class ExtentedToken
+    {
+        /// <summary>
+        /// Тип токена, функция, которую он выполняет
+        /// </summary>
+        public enum TYPE
+        {
+            BIN_OP,
+            OP,
+            LEFT_B,
+            RIGHT_B,
+        }
+        //
+        private TYPE _type;
+        /// <summary>
+        /// Тип расширенного токена
+        /// </summary>
+        public TYPE Type
+        {
+            get => _type;
+        }
+
+        private int _priority;
+        /// <summary>
+        /// Приоритет бинарной операции в случае, если токен ее отображает
+        /// </summary>
+        public int Priority
+        {
+            get => _priority;
+        }
+        /// <summary>
+        /// Значение токена при данном х
+        /// </summary>
+        public Func<double, double> Val;
+        
+        private string _stringVal;
+        /// <summary>
+        /// Строковое значение токена
+        /// </summary>
+        public string StringVal
+        {
+            get => _stringVal;
+        }
+        /// <summary>
+        /// Конструктор токена-оператора
+        /// </summary>
+        /// <param name="function">Функция, лежащая в основе оператора</param>
+        private ExtentedToken(Func<double, double> function) // OP
+        {
+            this.Val = function;
+            _type = TYPE.OP;
+            _stringVal = function.ToString();
+        }
+        /// <summary>
+        /// Конструктор токена-оператора
+        /// </summary>
+        /// <param name="op">Оператор, лежащий в основе токена</param>
+        private ExtentedToken(Operator op) // для OP
+        {
+            _type = TYPE.OP;
+            Val = new(op.CactAt);
+            _stringVal = op.ToString();
+        }
+        /// <summary>
+        /// Конструктор для токена-бинарного оператора
+        /// </summary>
+        /// <param name="priority">Приоритет бинарного оператора</param>
+        /// <param name="s">строка, представляющая бинарный оператор</param>
+        private ExtentedToken(int priority, string s) // для BIN_OP
+        {
+            _type = TYPE.BIN_OP;
+            _priority = priority;
+            _stringVal = s;
+        }
+        /// <summary>
+        /// Конструктор для токена-скобки
+        /// </summary>
+        /// <param name="token">Токен-скобка</param>
+        /// <param name="balance">Уровень вложенности скобки</param>
+        private ExtentedToken(Token token, int balance) //для скобок
+        {
+            _type = (TYPE)((int)token.Type-3);
+            _stringVal = token.TokenString;
+            _priority = balance;
+        }
+        /// <summary>
+        /// Превращает данный токен в расширенный
+        /// </summary>
+        /// <param name="token">Данный токен</param>
+        /// <returns>Расширенный токен</returns>
+        public static ExtentedToken ConvertFromToken(Token token)
+        {
+            if (token.Type == Token.TYPE.R_BRACE || token.Type == Token.TYPE.L_BRACE)
+                return new ExtentedToken(token, 0);
+            else if (token.Type == Token.TYPE.BINARY_OPERATOR)
+                return new ExtentedToken(DefinePriority(token), token.TokenString);
+            else 
+                return new ExtentedToken(new Operator(new List<Token>() { token }));
+        }
+        /// <summary>
+        /// Превращает последовательность выражение, состоящее из алгребраической коомбинации
+        /// сложных функций и констант в расширенный токен
+        /// </summary>
+        /// <param name="expression">Выражение в виде строки/param>
+        /// <returns>Токен - отражение выражения</returns>
+        public static ExtentedToken ConvertFromExpression(string expression)
+        {
+            ExtentedToken[] tokens = ToExtentedTokenArr(expression);
+            return ConvertFromExpression(tokens);
+        }
+        /// <summary>
+        /// Превращает последовательность выражение, состоящее из алгребраической коомбинации
+        /// токенов - сложных функций и констант в расширенный токен
+        /// </summary>
+        /// <param name="expression">Выражение в виде последовательности токенов/param>
+        /// <returns>Токен - отражение выражения</returns>
+        public static ExtentedToken ConvertFromExpression(IEnumerable<Token> expression)
+        {
+            ExtentedToken[] tokens = ToExtentedTokenArr(expression);
+            return ConvertFromExpression(tokens);
+        }
+        /// <summary>
+        /// Превращает последовательность выражение, состоящее из алгребраической коомбинации
+        /// расширенных токенов типа сложных функций и констант в расширенный токен
+        /// </summary>
+        /// <param name="expression">Выражение в виде последовательности расширенных токенов/param>
+        /// <returns>Токен - отражение выражения</returns>
+        public static ExtentedToken ConvertFromExpression(IEnumerable<ExtentedToken> expression)
+        {
+            Performer performer = new(expression);
+            performer.Parse();
+            return new((x) => performer.CalcAt(x));
+        }
+        /// <summary>
+        /// Превращает последовательность токенов в массив расширенных токенов
+        /// </summary>
+        /// <param name="expression">последовательность токенов</param>
+        /// <returns>массив расширенных токенов</returns>
+        public static ExtentedToken[] ToExtentedTokenArr(IEnumerable<Token> expression)
+        {
+            Token[] tokens = expression.ToArray();
+
+            List<ExtentedToken> operators = new List<ExtentedToken>();
+
+            List<Token> operatorList = new();
+
+            int level = 0;
+            for (int i = 0; i < tokens.Length; i++)
+            {
+
+                if (tokens[i].Type == Token.TYPE.L_BRACE)
+                {
+                    level++;
+                    operators.Add(new ExtentedToken(tokens[i], level));
+                    continue;
+                }
+                if (tokens[i].Type == Token.TYPE.R_BRACE)
+                {
+                    level--;
+                    operators.Add(new ExtentedToken(tokens[i], level));
+                    continue;
+                }
+                if (tokens[i].Type == Token.TYPE.UNARY_OPERATOR ||
+                    tokens[i].Type == Token.TYPE.FUNCTION ||
+                    tokens[i].Type == Token.TYPE.INT_NUM ||
+                    tokens[i].Type == Token.TYPE.VARIABLE ||
+                    tokens[i].Type == Token.TYPE.FLOAT_NUM)
+                {
+
+                    int targetLevel = level;
+                    operatorList.Add(tokens[i]);
+
+                    if (tokens[i].Type == Token.TYPE.FUNCTION || tokens[i].Type == Token.TYPE.UNARY_OPERATOR)
+                    {
+                        // Парсинг внутренней
+                        do
+                        {
+                            if (i < tokens.Length - 1)
+                                i++;
+                            else
+                                break;
+                            if (tokens[i].Type == Token.TYPE.L_BRACE)
+                            {
+                                level++;
+                                operatorList.Add(tokens[i]);
+                            }
+                            if (tokens[i].Type == Token.TYPE.R_BRACE)
+                            {
+                                level--;
+                                operatorList.Add(tokens[i]);
+                            }
+                            if (tokens[i].Type == Token.TYPE.UNARY_OPERATOR ||
+                                tokens[i].Type == Token.TYPE.FUNCTION ||
+                                tokens[i].Type == Token.TYPE.INT_NUM ||
+                                tokens[i].Type == Token.TYPE.VARIABLE ||
+                                tokens[i].Type == Token.TYPE.FLOAT_NUM)
+                            {
+                                operatorList.Add(tokens[i]);
+                            }
+                            if (level == targetLevel)
+                                break;
+                        } while (true);
+                    }
+
+                    Token[] opList = new Token[operatorList.Count];
+                    operatorList.CopyTo(opList);
+                    operators.Add(new ExtentedToken(new Operator(opList)));
+                    operatorList.Clear();
+                }
+                if (tokens[i].Type == Token.TYPE.BINARY_OPERATOR)
+                {
+                    operators.Add(new ExtentedToken(DefinePriority(tokens[i]), tokens[i].TokenString));
+                }
+            }
+            return operators.ToArray();
+
+        }
+        /// <summary>
+        /// Превращает строку в последовательность токенов
+        /// </summary>
+        /// <param name="str">Строка</param>
+        /// <returns>Массив токено</returns>
+        public static ExtentedToken[] ToExtentedTokenArr(string str)
+        {
+            Token[] tokens = Token.Tokenize(str);
+            return ToExtentedTokenArr(tokens);
+            
+        }
+        /// <summary>
+        /// Объединяет два токена, вызывая внутренний как аргумент внешнего
+        /// </summary>
+        /// <param name="origin">Внутренний токен</param>
+        /// <param name="child">Внешний токен, в который в качестве аргумента передается внешний</param>
+        /// <returns>Токен, объединивший внутренний и внешний</returns>
+        public static ExtentedToken Merge(ExtentedToken origin, ExtentedToken child)
+        {
+            return new((x) => child.Val(origin.Val(x)));
+        }
+        /// <summary>
+        /// Определяет приоритет токена-операции
+        /// </summary>
+        /// <param name="token">Токен</param>
+        /// <returns>Приоритет операции в токене</returns>
+        private static int DefinePriority(Token token)
+        {
+            return token.TokenString switch
+            {
+                "+" or "-" => 1,
+                "/" or "*" => 3,
+                "^" => 5,
+                "%" => 7,
+                _ => -1,
+            };
+        }
+        /// <summary>
+        /// Объединяет два токена в один на основе бинарной операции между ними
+        /// </summary>
+        /// <param name="first">Первый токен-операнд</param>
+        /// <param name="second">Второй токен-операнд</param>
+        /// <param name="operation">Бинарная операция между токенами</param>
+        /// <returns>Токен, объединяющий два других на основе бинарной операции</returns>
+        /// <exception cref="Exception">Исключение, возникающее в случае, если 
+        /// токены невозможно объединить</exception>
+        public static ExtentedToken Unify(ExtentedToken first, ExtentedToken second, ExtentedToken operation)
+        {
+            if (operation.Type != TYPE.BIN_OP 
+                || first.Type != TYPE.OP || second.Type != TYPE.OP)
+                throw new Exception("!!");
+
+            switch (operation.StringVal) 
+            {
+                case "+":
+                    return new ExtentedToken((x) => first.Val(x) + second.Val(x));
+                case "-":
+                    return new ExtentedToken((x) => first.Val(x) - second.Val(x));
+                case "*":
+                    return new ExtentedToken((x) => first.Val(x) * second.Val(x));
+                case "/":
+                    return new ExtentedToken((x) => first.Val(x) / second.Val(x));
+                case "^":
+                    return new ExtentedToken((x) => Math.Pow(first.Val(x), second.Val(x)));
+                case "%":
+                    return new ExtentedToken((x) => ((long)first.Val(x) % (long)second.Val(x)));
+            }
+            throw new Exception("!!");
+
+        }
+        /// <summary>
+        /// Возвращает строку, отобращающую данный токен
+        /// </summary>
+        /// <returns>Строка, отображающая данный токен</returns>
+        public override string ToString()
+        {
+            if (_type == TYPE.BIN_OP)
+            {
+                return $"BINARY OPERATOR : {_stringVal} : PRIORITY {_priority}";
+            } 
+            else if (_type == TYPE.LEFT_B)
+            {
+                return $"LEFT BRACE : {_stringVal} : {_priority}";
+            }
+            else if (_type == TYPE.RIGHT_B)
+            {
+                return $"RIGHT BRACE : {_stringVal} : {_priority}";
+            }
+            else
+            {
+                return $"OPERATOR : {_stringVal} : {Val.ToString()}";
+            }
+        }
+    }
+}
