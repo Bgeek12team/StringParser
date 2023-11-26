5971046f6d1a95e7a8d40fe7fa12ab02e7148ada
gr1fonn <147939718+gr1fonn@users.noreply.github.com>
2023-11-26 22:16:31 +0300

    Тесты: начало

---
+++ ParserTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using expressionParser;
+namespace ParserTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
